19605305c4c491080dc55029f7a464195852fa20
Thomas Brittain <honeysucklehardware@gmail.com>
2017-05-23 09:42:10 -0500

    Changed file names

--- js-ble.js
+++ js-ble.js
@@ -6,8 +6,9 @@ var receivedString = "";
 var terminalLineCounter = 0;
 var displayDOM = 'terminal';
 
-let primaryService = document.getElementById('optionalServices').value
+let primaryService = document.getElementById('optionalServices').value;
 
+var terminal = new Terminal(displayDOM);
 
 function onScanButtonClick() {
   lumiBle.searchAndConnect(0xFFE0);
@@ -32,25 +33,7 @@ function onWriteButtonClick(){
     }
 }
 
-function addTerminalLine(displayElement, text, pretext, lineStyle){
-    
-    // 1. Get the element to display text on.
-    // 2. Create a div for the new line.
-    // 3. Concactenate pre-text and text.
-    // 4. Check for style, apply
-    // 5. Append the new line DIV to target element.
-    // 6. Increment line count.
-    
-    var terminal = document.getElementById(displayElement);
-    var newLine = document.createElement('div');
-    newLine.innerHTML = pretext + text;
-    if(lineStyle != "") { 
-       newLine.classList.add(lineStyle); 
-    }
-    terminal.appendChild(newLine);
-    terminal.scrollTop = terminal.scrollHeight;
-    terminalLineCounter++;    
-}
+
 
 /* Utils */
 
