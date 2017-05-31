cfe948f472177ff038fe3662cab55c89ac0e3c87
Thomas Brittain <honeysucklehardware@gmail.com>
2017-05-31 07:56:24 -0500

    Converted Terminal LumiCommBLE to modules

--- js-ble.js
+++ js-ble.js
@@ -8,44 +8,44 @@ var displayDOM = 'terminal';
 
 let primaryService = document.getElementById('optionalServices').value;
 
-var terminal = new Terminal(displayDOM);
-
 function onScanButtonClick() {
-    lumiBle.searchAndConnect(0xFFE0).
+    lumiBle.searchAndConnect(parseInt(primaryService)).
     then(() => {
-        lumiBle.addReceivedDataCallback(onReceivedData)     
+        lumiBle.addReceivedDataCallback(onReceivedData)
     })
 }
 
-function onReceivedData(event){
-    for(var i = 0; i < event.target.value.byteLength; i ++){
+function onReceivedData(event) {
+    for (var i = 0; i < event.target.value.byteLength; i++) {
         receivedString += String.fromCharCode(event.target.value.getUint8(i));
     }
     terminal.addTerminalLine(displayDOM, receivedString, '<- ', 'received-text');
     receivedString = "";
 }
 
-function onWriteButtonClick(){
+function onWriteButtonClick() {
     let textToWrite = document.getElementById('textToWrite').value;
-//        writeCharacteristic.writeValue(encoder.encode(textToWrite))
+    //        writeCharacteristic.writeValue(encoder.encode(textToWrite))
     lumiBle.writeData(textToWrite)
-    .then(_ => {
-        terminal.addTerminalLine(displayDOM, textToWrite, '-> ', 'sent-text');
-    })
+        .then(_ => {
+            terminal.addTerminalLine(displayDOM, textToWrite, '-> ', 'sent-text');
+        })
 }
 
 /* Utils */
 function getSupportedProperties(characteristic) {
-  let supportedProperties = [];
-  for (const p in characteristic.properties) {
-    if (characteristic.properties[p] === true) {
-      supportedProperties.push(p.toUpperCase());
+    let supportedProperties = [];
+    for (const p in characteristic.properties) {
+        if (characteristic.properties[p] === true) {
+            supportedProperties.push(p.toUpperCase());
+        }
     }
-  }
-  return '[' + supportedProperties.join(', ') + ']';
+    return '[' + supportedProperties.join(', ') + ']';
 }
 
 document.getElementById('btn-1').onclick = onScanButtonClick;
 document.getElementById('btn-write-ble').onclick = onWriteButtonClick;
 
-var lumiBle = new LumiBluetooth();
+var lumiBle = LumiBluetooth;
+var terminal = Terminal;
+terminal.setDisplayDOM(displayDOM);
